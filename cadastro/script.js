1cb42d662030843d0464302dde5b61143fcee6cd
thigo47 <vilaportilho@gmail.com>
2023-03-15 22:36:37 +0000

    adicionei a interaçao do cadstro guardando as informaçoes recebidas

---
+++ cadastro/script.js
@@ -0,0 +1,30 @@
+let i = {
+    email:document.getElementById("emailc"),
+    senha:document.getElementById("senhac"),
+    res: document.getElementById("resp"),
+}
+  let cadastrado = []
+ function cadastro(params) {
+    if (i.email.value.length == 0 || i.email.value.length == 0) {
+        window.alert("Adicione valores antes de cadastrar")
+    } else {
+       
+        cadastrado.push(i.email.value , i.senha.value)
+        
+        let link = document.createElement("a")
+        link.href = "../Login/index.html"
+        link.id = "outrapag"
+
+        sessao.appendChild(link)
+
+        let novobotao = document.createElement("button")
+        novobotao.id = "login"
+        novobotao.innerHTML = "login"
+        outrapag.appendChild(novobotao)
+    }
+    
+
+
+}
+
+/* */
