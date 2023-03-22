b94b2b2baf93259b99570a1d53e1a97061aeff73
thigo47 <vilaportilho@gmail.com>
2023-03-22 20:21:24 +0000

    att

---
+++ cadastro mais detalhado/script.js
@@ -0,0 +1,16 @@
+let ip = {
+    nome: document.getElementById("nome"),
+    sobrenome: document.getElementById("sobrenome"),
+    nacio: document.getElementById("nacio"),
+    tel : document.getElementById("tel"),
+    nasc : document.getElementById("nasc"),
+}
+
+let ic = {
+    email:document.getElementById("email"),
+    senha: document.getElementById("senha"),
+    senhac : document.getElementById("confirmaçaosenha"),
+}
+function cadastro() {
+    
+}
