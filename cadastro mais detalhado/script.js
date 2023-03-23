34f0e01d9bc96b48ea3ce91493168d30ba5f4ddf
thigo47 <vilaportilho@gmail.com>
2023-03-23 01:21:34 +0000

    att do cadastro

--- cadastro mais detalhado/script.js
+++ cadastro mais detalhado/script.js
@@ -12,5 +12,25 @@ let ic = {
     senhac : document.getElementById("confirmaçaosenha"),
 }
 function cadastro() {
-    
+    if (ip.nome.value == 0 || ip.sobrenome.value == 0 || ip.nacio.value == 0 || ip.tel.value == 0 || ip.nasc.value == 0) {
+        window.alert("Adicione suas informaçoes antes de cadastrar")
+    } else if(ip.nasc.value >= "2009") {
+        window.alert("voce é novo demais para se cadastrar")
+    }else if (ic.senha.value != ic.senhac.value){
+        window.alert("a senha precia ser igual")
+    }else{
+        let sucesso = document.getElementById("sucesso")
+        let h2 = document.createElement("h2")
+        h2.innerHTML= `Olá ${ip.nome.value} ${ip.sobrenome.value} , voce foi cadastrado com sucesso`
+
+        sucesso.appendChild(h2)
+        //
+
+        let link = document.createElement("a")
+        link.href = "../Login/index.html"
+        link.innerText= "ir para login"
+
+        sucesso.appendChild(link)
+
+    }
 }
